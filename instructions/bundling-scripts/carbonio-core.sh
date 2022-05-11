ed2eb40807b6045222a1500409374bb586090ed1
M0Rf30 <morf3089@gmail.com>
2022-05-11 10:42:48 +0200

    chore: add postfix-prometheus script facility

--- instructions/bundling-scripts/carbonio-core.sh
+++ instructions/bundling-scripts/carbonio-core.sh
@@ -155,7 +155,8 @@ main()
    Copy ${repoDir}/zm-core-utils/src/libexec/configrewrite                                          ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/configrewrite
    Copy ${repoDir}/zm-core-utils/src/libexec/icalmig                                                ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/icalmig
    Copy ${repoDir}/zm-core-utils/src/libexec/zcs                                                    ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/zcs
-   Copy ${repoDir}/zm-core-utils/src/libexec/carbonio                                                 ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/carbonio
+   Copy ${repoDir}/zm-core-utils/src/libexec/carbonio                                               ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/carbonio
+   Copy ${repoDir}/zm-core-utils/src/libexec/postfix-prometheus                                     ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/postfix-prometheus
    Copy ${repoDir}/zm-core-utils/src/libexec/zmaltermimeconfig                                      ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/zmaltermimeconfig
    Copy ${repoDir}/zm-core-utils/src/libexec/zmantispamdbinit                                       ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/zmantispamdbinit
    Copy ${repoDir}/zm-core-utils/src/libexec/zmantispammycnf                                        ${repoDir}/zm-build/${currentPackage}/opt/zextras/libexec/zmantispammycnf
